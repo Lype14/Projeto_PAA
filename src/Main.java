1b31e6f4a2c5c15171bedfff28c2f47636abeaf1
Luis Felipe Revoredo <luis.revoredo@ufrpe.br>
2025-03-04 14:43:45 -0300

    Estruturas de dados(Vertices, Arestas e Grafo)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Grafo<Integer> grafo = new Grafo<Integer>();
+        grafo.adicionarVertice(1);
+        grafo.adicionarVertice(2);
+        grafo.adicionarVertice(3);
+        grafo.adicionarVertice(4);
+        grafo.adicionarVertice(5);
+       
+
+        grafo.adicionarAresta(2.0, 1, 2);
+        grafo.adicionarAresta(3.0, 2, 4);
+        grafo.adicionarAresta(1.0, 4, 3);
+        grafo.adicionarAresta(1.0, 1, 3);
+        grafo.adicionarAresta(2.0, 5, 2);
+        grafo.adicionarAresta(3.0, 5, 1);
     }
 }
