d22e8c9477dcf86f6b9c21582bb1d57ad6c0fc01
Luis Felipe Revoredo <luis.revoredo@ufrpe.br>
2025-03-05 00:42:19 -0300

    Função para atualizar o fluxo na rede principal de acordo com o caminho aumentante (a função ainda está incompleta).

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         Grafo<Integer> grafo = new Grafo<Integer>();
@@ -16,5 +18,26 @@ public class Main {
         grafo.adicionarAresta(3.0,0.0, 5, 1);
 
         grafo.buscaEmLargura(0,3);
+    }
+
+    public void edmondsKarp(){ // função que irá calcular o fluxo máximo para a rede
+
+    }
+
+    public <TIPO> Grafo aumentaFluxo(Grafo principal, Map<Vertice<TIPO>, Aresta<TIPO>> caminho,Vertice<TIPO>origem,Vertice<TIPO>destino){
+        // Passo 1: Determinar o fluxo máximo possível no caminho (mínimo das capacidades das arestas)
+    double fluxoMinimo = Double.MAX_VALUE;
+    Vertice<TIPO> atual = destino;
+
+    while (!atual.equals(origem)) {
+        Aresta<TIPO> aresta = caminho.get(atual);
+        if (aresta == null) {
+            System.out.println("Erro: Caminho inválido.");
+            return principal;
+        }
+        fluxoMinimo = Math.min(fluxoMinimo, aresta.getCapacidade());
+        atual = aresta.getInicio();
+    }
+        return principal;
     }
 }
